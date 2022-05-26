ec00e172e52577a30d20773dd7e0eea84069484e
sky-86 <sky86one@gmail.com>
2022-05-25 19:14:59 -0500

    video, audio, and image players/viewer proof of concept

---
+++ index.js
@@ -0,0 +1,47 @@
+// adds a event listener to the input that adds the video source
+// to the player
+function loadVideo() {
+    'use strict'
+    var playSelectedFile = function(event) {
+        var file = this.files[0]
+        var URL = window.URL || window.webkitURL 
+        var fileURL = URL.createObjectURL(file)
+        var videoNode = document.querySelector('.video-player')
+        videoNode.src = fileURL
+    }
+
+    var inputNode = document.querySelector('.in-video')
+    inputNode.addEventListener('change', playSelectedFile, false)
+}
+
+function loadAudio() {
+    var playSelectedFile = function(event) {
+        var file = this.files[0]
+        var URL = window.URL || window.webkitURL 
+        var fileURL = URL.createObjectURL(file)
+        var audioNode = document.querySelector('.audio-player')
+        audioNode.src = fileURL
+    }
+
+    var inputNode = document.querySelector('.in-audio')
+    inputNode.addEventListener('change', playSelectedFile, false)
+}
+
+function loadImage() {
+    var viewSelectedFile = function(event) {
+        var file = this.files[0]
+        var URL = window.URL || window.webkitURL 
+        var fileURL = URL.createObjectURL(file)
+        var imageNode = document.querySelector('.image-viewer')
+        imageNode.src = fileURL
+    }
+
+    var inputNode = document.querySelector('.in-image')
+    inputNode.addEventListener('change', viewSelectedFile, false)
+
+}
+
+loadVideo()
+loadAudio()
+loadImage()
+
