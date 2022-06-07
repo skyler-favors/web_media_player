9d4f324794e9a240c20bc1b7f6352deb1a1d31d4
sky-86 <sky86one@gmail.com>
2022-06-07 11:09:57 -0500

    hide video, audio, and img when not being used

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 // adds a event listener to the input that adds the video source
 // to the player
 function loadVideo() {
-    'use strict'
     var playSelectedFile = function(event) {
-        alert('im here now')
         var file = this.files[0]
         var URL = window.URL || window.webkitURL 
         var fileURL = URL.createObjectURL(file)
         var videoNode = document.querySelector('.video-player')
+
+        if (extension != "mp4") {
+        videoNode.src = null;
+        videoNode.style.display = "none"
+        return
+        }
+
+        videoNode.style.display = "initial"
         videoNode.src = fileURL
         document.querySelector(".video-player").style.visibility = "visible";
     }
     
     var inputNode = document.querySelector('.input-file')
     inputNode.addEventListener('change', playSelectedFile, false)
-    alert('im here')
 }
 
 function loadImage() {
     var viewSelectedFile = function(event) {
+    
         var file = this.files[0]
         var URL = window.URL || window.webkitURL 
         var fileURL = URL.createObjectURL(file)
         var imageNode = document.querySelector('.image-viewer')
+
+        if (extension != "png") {
+           imageNode.src = null
+           imageNode.style.display = "none"
+           return
+        }
+
+        imageNode.style.display = "initial"
         imageNode.src = fileURL
         document.querySelector(".image-viewer").style.visibility = "visible";
     }
@@ -32,18 +46,18 @@ function loadImage() {
 
 }
 
+let extension = "";
+
 function checkFileExtension() {
     fileName = document.querySelector('#choose-file').value;
     //split extension path into substrings and pops the last element of the array off
     extension = fileName.split('.').pop();
     extension = extension.toLowerCase();
     console.log(extension) 
-
-    if (extension == 'mov') {
-        loadVideo();
-    }
-
 };
+
+loadVideo()
+loadImage()
 
 
 
